e100fe9f7e9722b65730e9a9885a93353f2ac66c
YuKono <cosmos17128@gmail.com>
2021-05-14 21:06:11 +0900

    swiftファイルの作成まで

---
+++ WordList/QuesrionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuesrionViewController.swift
+//  WordList
+//
+//  Created by Yu Kono on 2021/05/14.
+//
+
+import UIKit
+
+class QuesrionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
