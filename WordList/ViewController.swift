f7aaaf29688dd67ace5a41d339ab5956707f6625
YuKono <cosmos17128@gmail.com>
2021-05-14 20:19:05 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Yu Kono on 2021/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
